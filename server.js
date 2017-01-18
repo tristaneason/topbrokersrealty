816fb181e5fe3c12caf7c37bb1552782570ed631
Tristan Eason <tristaneason@protonmail.com>
2017-01-17 22:53:12 -0500

    Revert "Changes to be reverted"
    This reverts commit 6bcca3986abb440055feb175a10ea6df44c581b6.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const favicon = require("serve-favicon")
-const sendmail = require("sendmail")
 
 const cssAssets = __dirname + "/public/css"
 const imgAssets = __dirname + "/public/images"
@@ -20,8 +19,4 @@ express()
   .get("/privacy-policy", (req, res, next) => {
     res.render("privacy")
   })
-  .post("/send-info", (req, res, next) => {
-    const { name } = req.body
-    const { phone } = req.body
-    // res.send(`You sent ${req.body.name} and ${req.body.phone}.`)
   .listen(3000)
