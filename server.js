6bcca3986abb440055feb175a10ea6df44c581b6
Tristan Eason <tristaneason@protonmail.com>
2017-01-17 22:53:08 -0500

    Changes to be reverted

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const favicon = require("serve-favicon")
+const sendmail = require("sendmail")
 
 const cssAssets = __dirname + "/public/css"
 const imgAssets = __dirname + "/public/images"
@@ -19,4 +20,8 @@ express()
   .get("/privacy-policy", (req, res, next) => {
     res.render("privacy")
   })
+  .post("/send-info", (req, res, next) => {
+    const { name } = req.body
+    const { phone } = req.body
+    // res.send(`You sent ${req.body.name} and ${req.body.phone}.`)
   .listen(3000)
